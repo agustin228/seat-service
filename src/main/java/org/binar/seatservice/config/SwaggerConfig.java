ee4106281b010bdf4742b35ecf2d68a6cac9ec42
Agustinus <agustin22.tm@gmail.com>
2022-12-29 13:31:56 +0700

    Add endpoint deployment to swagger config

--- src/main/java/org/binar/seatservice/config/SwaggerConfig.java
+++ src/main/java/org/binar/seatservice/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public OpenAPI demoAPI(@Value("REST API Seat Microservice for Bioskop API ") String appDescription,
                            @Value("v1.0.0") String appVersion){
         Server serverLocal = new Server();
-        serverLocal.setUrl("https://filmservice-production-36c0.up.railway.app");
+        serverLocal.setUrl("https://seat-service-production-2489.up.railway.app");
         List<Server> listOfServer = new ArrayList<>();
         listOfServer.add(serverLocal);
 
